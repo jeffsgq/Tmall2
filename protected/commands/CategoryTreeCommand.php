8e1a8077879025cf95d5a2d90ea6ade60611099f
jeffsgq <jeffsgq@163.com>
2015-03-26 09:08:09 +0800

    3/26
    update

--- protected/commands/CategoryTreeCommand.php
+++ protected/commands/CategoryTreeCommand.php
@@ -1,28 +1,19 @@
 <?php
-Yii::$enableIncludePath = false; 
 Yii::import('application.components.TaobaoConnectorSQL');
-Yii::import('application.extensions.PHPExcel.PHPExcel', 1);
 require_once( dirname(__FILE__) . '/../components/ConsoleCommand.php' );
-include_once (dirname(__FILE__).'/../extensions/PHPExcel/PHPExcel/IOFactory.php');
 class CategoryTreeCommand extends ConsoleCommand {
-    protected $PHPExcel = null;
-    protected $saveFileName = null;
     protected $_className= null ;
     protected $parentid_Array = null;
     public function init(){
-        $this->PHPExcel = new PHPExcel();
-        $this->saveFileName = dirname(__FILE__).'/../../Excel/php.xls';
         $this->_className= get_class() ;
         $this->beforeAction( $this->_className, '') ;
-        //创建php.xls
-        fopen($this->saveFileName, "w+");
+        //数组初始化
+        $this->parentid_Array = array("0");
     }
-    
     public function run($args){
         $this->_clearMysql('0_parentcid');
         $this->_Print();
     }
-
     //获取API属性
     public function _getAPIValue($parent_cid){
         $_itemsTmallAll= array();
@@ -32,68 +23,38 @@ class CategoryTreeCommand extends ConsoleCommand {
         }
         return $_itemsTmallAll;
     }
-    
     //使用递归调用
     public function _insertDB($parent_cid,$i){
         //获取API属性
         $_itemsTmallAll = $this->_getAPIValue($parent_cid);
-        ob_start();
         $_items = $this->_formatArray($_itemsTmallAll);
         $_items2 = $this->_jsonNbFormat($_items);
         foreach ($_items2 as $_firstKey=>$_firstValue){
-            $j = 65;
             if(strcasecmp($_firstValue['is_parent'],1)==0){
                 $is_parent = "true"; 
             }else{
                 $is_parent = "false"; 
             }
-            $this->_insertMysql($_firstValue['cid'],$is_parent,$_firstValue['name'], $_firstValue['parent_cid']);//,$_firstValue['is_parent'],$_firstValue['name'].""
+            $this->_insertMysql($_firstValue['cid'],$is_parent,$_firstValue['name'], $_firstValue['parent_cid']);
             if(strcasecmp($_firstValue['is_parent'],1)==0){
-               foreach ($_firstValue as $_secnodKey=> $_secondValue){
-                    $this->PHPExcel->setActiveSheetIndex(0)->setCellValue(chr($j).$i, $_secondValue);
-                    $j = $j+1;
-                }
+                array_push($this->parentid_Array, $_firstValue['cid']);
                 $i = $i + 1;
-                //将
             }
         }
         return $i;
     }
-    
     //循环输出并保存在Excel中
     public function _Print(){
-        //数组初始化
-        $title = array("cid","is_parent","name","parent_cid");
-        ob_start();
-        $currentSheet = $this->PHPExcel->setactivesheetindex(0);
-        for($i=0,$index = 65;$i<count($title);$i++,$index++){
-            $currentSheet->setCellValue(chr($index)."1", $title[$i]);    
-        }
-        $currentSheet->setCellValue('A2', '0')//初始化
-                ->setTitle("Sheet1");
         //循环
-        $i=2;
-        $k=2;
-        //循环写入
+        $i=1;
+        $k=0;
         do{
-            $parent_cid = $this->PHPExcel->setActiveSheetIndex(0)->getCell('A'.$k)->getValue();
+            $parent_cid = $this->parentid_Array[$k];
             $i = $this->_insertDB($parent_cid, $i);
             $k = $k + 1;
         }while($k!=$i);
-        //保存Excel数据库
-        if(!is_writable($this->saveFileName)){
-            echo 'Can not Write';
-            return ;
-        }
-        header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename='.$this->saveFileName);
-        header('Cache-Control: max-age=0');
-        //创建文件使用Excel2003版本
-        $objWriter = PHPExcel_IOFactory::createWriter($this->PHPExcel,'Excel5');  
-        $objWriter->save($this->saveFileName);
         echo 'END';
     }
-   
     private function _formatArray($_items){
         $_itemsArray=array();
         foreach ($_items as $_firstKey=>$_firstValue){
@@ -103,7 +64,6 @@ class CategoryTreeCommand extends ConsoleCommand {
         }
         return $_itemsArray;
     }
-    
     private function _jsonNbFormat($_items){
         foreach ($_items as $key=>$val){
             $_items[$key]['cid']= number_format($val['cid'],0,'','');
@@ -111,7 +71,6 @@ class CategoryTreeCommand extends ConsoleCommand {
         }
         return $_items;
     }
-    
     private function _connectTmall($_sessionkey,$_parentid){
         $_taobaoConnect= new TaobaoConnectorSQL();
         $_taobaoConnect->__url=Yii::app()->params['taobao_api']['url'] ;
@@ -136,14 +95,12 @@ class CategoryTreeCommand extends ConsoleCommand {
             return false;
         }
     }
-
     private function _insertMysql($cid,$is_parent,$name,$parent_cid){
         $sql_insert = "insert into 0_parentcid(c_id,is_parent,name,parent_cid) values (" . $cid . "," . $is_parent . ",'" . $name . "'," . $parent_cid . ")";
         $connection= Yii::app()->db;//建立数据库连接
         $command2 = $connection->createCommand($sql_insert);
         $command2->execute();
     }
-    
     private function _clearMysql($tblName){
         $sql_clear = "TRUNCATE TABLE ".$tblName;
         $connection= Yii::app()->db;//建立数据库连接
